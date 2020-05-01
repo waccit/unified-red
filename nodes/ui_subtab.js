85935b94bb8a972b2cd5acb9be8c01c5d6ccc36a
Nick Lee <nlee@wasocal.com>
2020-04-30 22:42:41 -0700

    add ui_base, ui_tab, ui_subtab, ui_group with locale & icons

---
+++ nodes/ui_subtab.js
@@ -0,0 +1,22 @@
+module.exports = function(RED) {
+
+    function SubtabNode(config) {
+        RED.nodes.createNode(this, config);
+        this.config = {
+            name: config.name,
+            disp: config.disp,
+            width: config.width,
+            order: config.order || 0,
+            tab: config.tab,
+            collapse: config.collapse || false,
+            icon: config.icon || '',
+            disabled: config.disabled || false,
+            hidden: config.hidden || false
+        };
+        if (!this.config.hasOwnProperty("disp")) { this.config.disp = true; }
+        if (this.config.disp !== false) { this.config.disp = true; }
+        if (!this.config.hasOwnProperty("collapse")) { this.config.collapse = false; }
+    }
+
+    RED.nodes.registerType("ui_subtab", SubtabNode);
+};
