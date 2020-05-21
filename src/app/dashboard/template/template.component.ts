f8e6020a8fcc485e949e7d6266d2083e3f3ca878
Michael Montalvo <mmontalvo@wasocal.com>
2020-05-20 18:51:34 -0700

    Template Buildable

--- src/app/dashboard/template/template.component.ts
+++ src/app/dashboard/template/template.component.ts
@@ -1,54 +1,53 @@
 import { Component, OnInit } from "@angular/core";
 
 
-import { SocketIoService } from './../../services/socket-io.service';
+import { WebSocketService } from '../../services/web-socket.service';
 import { DynamicScriptLoaderService } from './../../services/dynamic-script-loader.service';
 
 @Component({
-  selector: 'app-template',
-  templateUrl: './template.component.html',
-  styleUrls: ['./template.component.scss']
+    selector: 'app-template',
+    templateUrl: './template.component.html',
+    styleUrls: ['./template.component.scss']
 })
 
 export class TemplateComponent implements OnInit {
-  constructor(private dynamicScriptLoader: DynamicScriptLoaderService, private socketIo: SocketIoService) {}
+    constructor(private dynamicScriptLoader: DynamicScriptLoaderService, private webSocketService: WebSocketService) {}
 
-  text1: any = {
-    id: '',
-    value: ''
-  };
+    text1: any = {
+        id: '',
+        value: ''
+    };
 
-  text2: any = {
-    id: '',
-    value: ''
-  };
+    text2: any = {
+        id: '',
+        value: ''
+    };
 
-  ngOnInit() {
-    'use strict';
+    ngOnInit() {
+        'use strict';
 
-    this.socketIo.connect( ui => {
-      console.log(ui.site);
-      console.log(ui.menu);
-    }, () => {});
+        this.webSocketService.listen('ui-controls').subscribe( (data) => {
+            console.log("WebSocketService connect data: ", data);
+          });
+      
+          this.webSocketService.listen('update-value').subscribe( (data: any) => {
+            console.log('update-value called: ', data);
+            if (data.value.length > 3) {
+              if (this.text1.id.length === 0 ) {
+                this.text1.id = data.id;
+                this.text1.value = data.value
+              }
+      
+              this.text1.value = data.value;
+            } else {
+              if (this.text2.id.length === 0) {
+                this.text2.id = data.id;
+                this.text2.value = data.value
+              }
+      
+              this.text2.value = data.value
+            }
+          });
 
-    this.socketIo.on('update-value', data => {
-      console.log('update-value called: ', data);
-      if (data.value.length > 3) {
-        if (this.text1.id.length === 0 ) {
-          this.text1.id = data.id;
-          this.text1.value = data.value
-        }
-
-        this.text1.value = data.value;
-      } else {
-        if (this.text2.id.length === 0) {
-          this.text2.id = data.id;
-          this.text2.value = data.value
-        }
-
-        this.text2.value = data.value
-      }
-    });
-
-  }
+    }
 }
