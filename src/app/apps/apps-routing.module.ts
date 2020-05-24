f1d052a196f53345cd40a1fc52717fd601fc75c8
Samy Arbid <sarbid@wasocal.com>
2020-05-24 10:12:01 -0700

    Moved several components that we'll probably never use to src-old

--- src/app/apps/apps-routing.module.ts
+++ src/app/apps/apps-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CalendarComponent } from './calendar/calendar.component';
-import { ChatComponent } from './chat/chat.component';
-import { ContactGridComponent } from './contact-grid/contact-grid.component';
-import { ContactListComponent } from './contact-list/contact-list.component';
+// import { ChatComponent } from './chat/chat.component';
+// import { ContactGridComponent } from './contact-grid/contact-grid.component';
+// import { ContactListComponent } from './contact-list/contact-list.component';
 import { SupportComponent } from './support/support.component';
 import { DragDropComponent } from './drag-drop/drag-drop.component';
 
@@ -12,18 +12,18 @@ const routes: Routes = [
     path: 'calendar',
     component: CalendarComponent
   },
-  {
-    path: 'chat',
-    component: ChatComponent
-  },
-  {
-    path: 'contact-grid',
-    component: ContactGridComponent
-  },
-  {
-    path: 'contact-list',
-    component: ContactListComponent
-  },
+  // {
+  //   path: 'chat',
+  //   component: ChatComponent
+  // },
+  // {
+  //   path: 'contact-grid',
+  //   component: ContactGridComponent
+  // },
+  // {
+  //   path: 'contact-list',
+  //   component: ContactListComponent
+  // },
   {
     path: 'support',
     component: SupportComponent
