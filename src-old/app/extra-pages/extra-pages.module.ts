50f1d39a03d171de7657f74e21b54f4a04fc5a85
Samy Arbid <sarbid@wasocal.com>
2020-05-27 19:53:24 -0700

    Moved more modules to src-old

--- src-old/app/extra-pages/extra-pages.module.ts
+++ src-old/app/extra-pages/extra-pages.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ExtraPagesRoutingModule } from './extra-pages-routing.module';
-import { ProfileComponent } from './profile/profile.component';
+// import { ProfileComponent } from './profile/profile.component';
 // import { PricingComponent } from './pricing/pricing.component';
 // import { InvoiceComponent } from './invoice/invoice.component';
 // import { FaqsComponent } from './faqs/faqs.component';
@@ -17,7 +17,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
-    ProfileComponent,
+    // ProfileComponent,
     // PricingComponent,
     // InvoiceComponent,
     // FaqsComponent,
