302f8a48ace56c253a79a3eb8c85acbc5b723235
Nick Lee <nlee@wasocal.com>
2020-05-28 10:32:19 -0700

    add ur_button component

---
+++ src/app/node-templates/ur-button/ur-button.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-ur-button',
+    templateUrl: './ur-button.component.html',
+    styleUrls: ['./ur-button.component.sass'],
+})
+export class UrButtonComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
