6e4cc3cf544c2e6bcbecd078c27d4ebd6a91d3ec
Nick Lee <nlee@wasocal.com>
2020-05-28 10:09:30 -0700

    add ur_button node

---
+++ nodes/ur_button.js
@@ -0,0 +1,81 @@
+module.exports = function (RED) {
+    var ui = require('../ui')(RED);
+
+    function ButtonNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        var group = RED.nodes.getNode(config.group);
+        if (!group) {
+            return;
+        }
+        var subtab = RED.nodes.getNode(group.config.subtab);
+        if (!subtab) {
+            return;
+        }
+        var tab = RED.nodes.getNode(subtab.config.tab);
+        if (!tab) {
+            return;
+        }
+
+        var payloadType = config.payloadType;
+        var payload = config.payload;
+
+        if (payloadType === 'flow' || payloadType === 'global') {
+            try {
+                var parts = RED.util.normalisePropertyExpression(payload);
+                if (parts.length === 0) {
+                    throw new Error();
+                }
+            } catch (err) {
+                node.warn(
+                    'Invalid payload property expression - defaulting to node id'
+                );
+                payload = node.id;
+                payloadType = 'str';
+            }
+        } else {
+            payload = payload || node.id;
+        }
+
+        var done = ui.add({
+            node: node,
+            tab: tab,
+            subtab: subtab,
+            group: group,
+            emitOnlyNewValues: false,
+            forwardInputMessages: config.passthru || false,
+            storeFrontEndInputAsState: false,
+            control: {
+                type: 'button',
+                label: config.label,
+                tooltip: config.tooltip,
+                color: config.color,
+                bgcolor: config.bgcolor,
+                icon: config.icon,
+                order: config.order,
+                value: payload,
+                format: config.bgcolor,
+                width: config.width || group.config.width || 3,
+                height: config.height || 1,
+            },
+            beforeSend: function (msg) {
+                msg.topic = config.topic || msg.topic;
+            },
+            convertBack: function (value) {
+                if (payloadType === 'date') {
+                    value = Date.now();
+                } else {
+                    value = RED.util.evaluateNodeProperty(
+                        payload,
+                        payloadType,
+                        node
+                    );
+                }
+                return value;
+            },
+        });
+        node.on('close', done);
+    }
+    RED.nodes.registerType('ur_button', ButtonNode);
+};
