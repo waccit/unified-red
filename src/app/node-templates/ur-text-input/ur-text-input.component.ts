6b2457e4097432f10870b5eaedab15d0b7434c87
Nick Lee <nlee@wasocal.com>
2020-06-11 23:29:24 -0700

    add ur-text-input UI component

---
+++ src/app/node-templates/ur-text-input/ur-text-input.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-ur-text-input',
+    templateUrl: './ur-text-input.component.html',
+    styleUrls: ['./ur-text-input.component.sass'],
+})
+export class UrTextInputComponent implements OnInit {
+    @Input() text: string;
+
+    constructor() {}
+
+    ngOnInit(): void {}
+}
