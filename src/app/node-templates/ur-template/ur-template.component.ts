11422a6254d7da4f3642fa77049aedf3328a094f
Samy Arbid <sarbid@wasocal.com>
2020-06-20 13:12:12 -0700

    ur-template node complete

---
+++ src/app/node-templates/ur-template/ur-template.component.ts
@@ -0,0 +1,38 @@
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { WebSocketService } from '../../services';
+
+declare var $: any;
+
+@Component({
+    selector: 'app-ur-template',
+    templateUrl: './ur-template.component.html',
+    styleUrls: ['./ur-template.component.sass'],
+})
+export class UrTemplateComponent implements AfterViewInit {
+    data: any;
+    @ViewChild('container', { static: true }) container: ElementRef;
+
+    constructor(private webSocketService: WebSocketService) {}
+
+    ngAfterViewInit(): void {
+        if (!this.data || !this.data.format) {
+            this.container.nativeElement.innerHTML = 'No template code found';
+            return;
+        }
+
+        this.webSocketService.listen('update-value').subscribe((data: any) => {
+            if (this.data.id == data.id && data.msg) {
+                // fire jQuery custom event
+                $(this.container.nativeElement).trigger('update-value', [data]);
+            }
+        });
+
+        // jQuery cannot handle ID's with periods. Must escape periods.
+        let nodeId = this.data.id.replace(/\./g, '\\\\.');
+        // Substite any node ID references in the template code
+        let html = this.data.format.replace(/\{\{id\}\}/g, nodeId);
+        // Use createContextualFragment to ensure user scripts execute
+        const executableCode = document.createRange().createContextualFragment(html);
+        this.container.nativeElement.appendChild(executableCode);
+    }
+}
