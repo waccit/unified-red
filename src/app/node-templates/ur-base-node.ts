8b5a53814ca6598263cebe3f9d16d7e4f5b090ca
Samy Arbid <sarbid@wasocal.com>
2020-06-21 09:19:32 -0700

    Added base node

---
+++ src/app/node-templates/ur-base-node.ts
@@ -0,0 +1,56 @@
+import { ElementRef, ViewChild, AfterViewInit } from "@angular/core";
+import { WebSocketService } from '../services';
+
+declare var $: any;
+
+export class BaseNode implements AfterViewInit {
+    label: string = 'default';
+    private _data: any;
+    @ViewChild('container', { static: true }) private _container: ElementRef;
+    
+    constructor(private webSocketService: WebSocketService) {}
+
+    ngAfterViewInit(): void {
+        this.webSocketService.listen('update-value').subscribe((msg: any) => {
+            if (msg && msg.id && this.nodeId == msg.id) {
+                this.updateValue(msg);
+            }
+        });
+
+        // Add send event to jQuery element
+        this.jQcontainer.on("send", (evt, msg) => {
+            this.send(msg);
+        });
+    }
+
+    get container() {
+        return this._container ? this._container.nativeElement : null;
+    }
+
+    get jQcontainer() {
+        return this._container ? $(this._container.nativeElement) : null;
+    }
+
+    get nodeId(): string {
+        return this.data ? this.data.id : null;
+    }
+
+    get data() {
+        return this._data;
+    }
+
+    set data(data:any) {
+        this._data = data;
+        this.label = this.data.label;
+    }
+
+    updateValue(msg:any) {
+        this.jQcontainer.trigger('update-value', [msg]);
+    }
+
+    send(msg:any) {
+        if (this.nodeId) {
+            this.webSocketService.emit('update-value', { id: this.nodeId, msg: msg });
+        }
+    }
+}
