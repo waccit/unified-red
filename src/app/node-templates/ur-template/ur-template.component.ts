85c92f3a63cbc4db8bfb0f5102f6188789056d09
Samy Arbid <sarbid@wasocal.com>
2020-06-21 09:19:32 -0700

    Added base node

--- src/app/node-templates/ur-template/ur-template.component.ts
+++ src/app/node-templates/ur-template/ur-template.component.ts
@@ -1,43 +1,28 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import { WebSocketService } from '../../services';
-
-declare var $: any;
+import { Component } from '@angular/core';
+import { BaseNode } from '../ur-base-node';
 
 @Component({
     selector: 'app-ur-template',
     templateUrl: './ur-template.component.html',
     styleUrls: ['./ur-template.component.sass'],
 })
-export class UrTemplateComponent implements AfterViewInit {
-    data: any;
-    @ViewChild('container', { static: true }) container: ElementRef;
-
-    constructor(private webSocketService: WebSocketService) {}
-
+export class UrTemplateComponent extends BaseNode {
     ngAfterViewInit(): void {
+        super.ngAfterViewInit();
         if (!this.data || !this.data.format) {
-            this.container.nativeElement.innerHTML = 'No template code found';
+            this.container.innerHTML = 'No template code found';
             return;
         }
+        this.appendHtml();
+    }
 
-        this.webSocketService.listen('update-value').subscribe((data: any) => {
-            if (this.data.id == data.id && data.msg) {
-                // fire jQuery custom event
-                $(this.container.nativeElement).trigger('update-value', [data]);
-            }
-        });
-
+    private appendHtml() {
         // jQuery cannot handle ID's with periods. Must escape periods.
-        let nodeId = this.data.id.replace(/\./g, '\\\\.');
+        let nodeId = this.nodeId.replace(/\./g, '\\\\.');
         // Substite any $node references in the template code
         let html = this.data.format.replace(/\$node/g, `$("#${nodeId}")`);
         // Use createContextualFragment to ensure user scripts execute
         const executableCode = document.createRange().createContextualFragment(html);
-        this.container.nativeElement.appendChild(executableCode);
-
-        // Add send event to DOM element
-        $(this.container.nativeElement).on("send", (evt, msg) => {
-            this.webSocketService.emit('update-value', { id: this.data.id, msg: msg });
-        });
+        this.container.appendChild(executableCode);
     }
 }
