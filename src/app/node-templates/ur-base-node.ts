b31a8702ab2505ccf0eaf5e7c69bd8f8b02eb948
Nick Lee <nlee@wasocal.com>
2020-07-08 14:55:13 -0700

    update BaseNode: add onDestroy and update emits

--- src/app/node-templates/ur-base-node.ts
+++ src/app/node-templates/ur-base-node.ts
@@ -1,26 +1,36 @@
-import { ElementRef, ViewChild, AfterViewInit } from "@angular/core";
+import { ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { WebSocketService } from '../services';
+import { Subscription } from 'rxjs';
 
 declare var $: any;
 
-export class BaseNode implements AfterViewInit {
+export class BaseNode implements AfterViewInit, OnDestroy {
     label: string = 'default';
     private _data: any;
+    private _wsSubscription: Subscription;
     @ViewChild('container', { static: true }) private _container: ElementRef;
-    
+
     constructor(private webSocketService: WebSocketService) {}
 
     ngAfterViewInit(): void {
-        this.webSocketService.listen('update-value').subscribe((msg: any) => {
+        this._wsSubscription = this.webSocketService.listen('update-value').subscribe((msg: any) => {
             if (msg && msg.id && this.nodeId == msg.id) {
                 this.updateValue(msg);
             }
         });
 
         // Add send event to jQuery element
-        this.container.on("send", (evt, msg) => {
+        this.container.on('send', (evt, msg) => {
             this.send(msg);
         });
+    }
+
+    ngOnDestroy(): void {
+        if (this._wsSubscription) {
+            this._wsSubscription.unsubscribe();
+        }
+
+        // console.log(this.data.type + ' destroyed');
     }
 
     get container() {
@@ -35,20 +45,20 @@ export class BaseNode implements AfterViewInit {
         return this._data;
     }
 
-    set data(data:any) {
+    set data(data: any) {
         this._data = data;
         this.label = this.data.label;
     }
 
-    updateValue(data:any) {
+    updateValue(data: any) {
         if (this.container && this.container.length) {
-            this.container.trigger('update-value', [data]);
+            this.container.trigger([data]);
         }
     }
 
-    send(msg:any) {
+    send(msg: any) {
         if (this.nodeId) {
-            this.webSocketService.emit('update-value', { id: this.nodeId, msg: msg });
+            this.webSocketService.emit({ id: this.nodeId, msg: msg });
         }
     }
 }
