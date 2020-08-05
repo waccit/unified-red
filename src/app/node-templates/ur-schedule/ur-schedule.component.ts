4ad766ef71b7c50ed8de96245a3e6b6c3cf08045
Samy Arbid <sarbid@wasocal.com>
2020-08-04 22:49:45 -0700

    Added schedule dialog form and started on event handling and deploying changes to NR

--- src/app/node-templates/ur-schedule/ur-schedule.component.ts
+++ src/app/node-templates/ur-schedule/ur-schedule.component.ts
@@ -1,7 +1,10 @@
 import { Component, ViewChild } from '@angular/core';
-import { FullCalendarComponent, CalendarOptions } from '@fullcalendar/angular';
+import { FullCalendarComponent, CalendarOptions, compareByFieldSpec } from '@fullcalendar/angular';
 import { BaseNode } from '../ur-base-node';
 import * as parser from 'cron-parser';
+import { UrScheduleFormDialogComponent } from './ur-schedule-form-dialog.component';
+import { WebSocketService, SnackbarService, NodeRedApiService } from '../../services';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
     selector: 'app-ur-schedule',
@@ -22,9 +25,23 @@ export class UrScheduleComponent extends BaseNode {
         events: [],
         editable: true,
         weekends: true,
-        dateClick: this.handleDateClick.bind(this), // bind is important!
-        eventClick: this.handleEventClick.bind(this), // bind is important!
+         // Important: Bind all event handlers to UrScheduleComponent
+        dateClick: this.handleDateClick.bind(this),
+        eventClick: this.handleEventClick.bind(this),
+        eventResize: this.handleEventResize.bind(this),
+        eventDrop: this.handleEventDrop.bind(this),
     };
+
+    dirty = false;
+
+    constructor(
+        protected webSocketService: WebSocketService,
+        public dialog: MatDialog,
+        private snackbar: SnackbarService,
+        private red : NodeRedApiService
+    ) {
+        super(webSocketService);
+    }
 
     ngAfterViewInit(): void {
         super.ngAfterViewInit();
@@ -87,11 +104,10 @@ export class UrScheduleComponent extends BaseNode {
                                 daysOfWeek: [ weekday ],
                                 groupId: "weekday"+weekday, // assign each weekday to a group so changes to a weekday are repeated every week
                                 startTime: `${ start.hour.padStart(2, "0") }:${ start.minute.padStart(2, "0") }:00`,
-                                startPattern: start.pattern,
                                 // if no end event was defined, then set end to end of day
                                 endTime: end ? `${ end.hour.padStart(2, "0") }:${ end.minute.padStart(2, "0") }:00` : '23:59:59',
-                                endPattern: end ? end.pattern : null,
-                                classNames: ["ur-schedule-weekday-event"]
+                                classNames: ["ur-schedule-weekday-event"],
+                                schedule: { start: start, end: end, values: this.data.values, type: "weekday" }
                             };
                             events.push(event);
                         } catch (err) {
@@ -139,12 +155,11 @@ export class UrScheduleComponent extends BaseNode {
                                 title: start.value,
                                 allDay: false,
                                 start: new Date(year, month, date, start.hour, start.minute),
-                                startPattern: start.pattern,
                                 // if no end event was defined, then set end to end of day
                                 end: end ? new Date(year, month, date, end.hour, end.minute) 
                                     : new Date(year, month, date, 23, 59, 59),
-                                endPattern: end ? end.pattern : null,
-                                classNames: ["ur-schedule-date-event"]
+                                classNames: ["ur-schedule-date-event"],
+                                schedule: { start: start, end: end, values: this.data.values, type: "date" }
                             };
                             events.push(event);
                         } catch (err) {
@@ -206,12 +221,11 @@ export class UrScheduleComponent extends BaseNode {
                                 title: start.value,
                                 allDay: false,
                                 start: new Date(year, month, date, start.hour, start.minute),
-                                startPattern: start.pattern,
                                 // if no end event was defined, then set end to end of day
                                 end: end ? new Date(year, month, date, end.hour, end.minute) 
                                     : new Date(year, month, date, 23, 59, 59),
-                                endPattern: end ? end.pattern : null,
-                                classNames: ["ur-schedule-holiday-event"]
+                                classNames: ["ur-schedule-holiday-event"],
+                                schedule: { start: start, end: end, values: this.data.values, type: "holiday" }
                             };
                             events.push(event);
                         } catch (err) {
@@ -226,12 +240,99 @@ export class UrScheduleComponent extends BaseNode {
         return events;
     }
 
-    handleDateClick(arg) {
-        console.log("handleDateClick", arg);
+    handleDateClick(data) {
+        console.log("handleDateClick", data);
+        let dialogData = {
+            data: { start: null, end: null, values: this.data.values, type: "weekday" },
+            action: 'add'
+        };
+        // if click was on week grid, then assume the user wants to add a weekday schedule,
+        // if click was on the day/month grid, then assume the user wants to add a date schedule. 
+        switch (data.view.type) {
+            case "timeGridWeek":
+                dialogData.data.type = "weekday";
+                dialogData.data.start = {
+                    weekday: data.date.getDay(),
+                    value: this.data.values[0]?.name,
+                    hour: data.date.getHours(),
+                    minute: data.date.getMinutes().toString().padStart(2, "0")
+                };
+                break;
+            case "dayGridDay":
+            case "dayGridMonth":
+                dialogData.data.type = "date";
+                dialogData.data.start = {
+                    date: data.date.toString(),
+                    value: this.data.values[0]?.name,
+                    hour: data.date.getHours(),
+                    minute: data.date.getMinutes().toString().padStart(2, "0")
+                };
+                break;
+        }
+        this.dialog
+            .open(UrScheduleFormDialogComponent, { data: dialogData })
+            .afterClosed()
+            .subscribe((result) => {
+                console.log("add response", result);
+                if (result) {
+                    this.dirty = true;
+                    // this.roleService.update(row.level, result).subscribe(
+                    //     (data) => {
+                    //         this.snackbar.success('Added user successfully!');
+                    //         this.refreshTable();
+                    //     },
+                    //     (error) => {
+                    //         this.snackbar.error(error);
+                    //     }
+                    // );
+                }
+            });
     }
 
-    handleEventClick(arg) {
-        console.log("handleEventClick", arg);
+    handleEventClick(data) {
+        console.log("handleEventClick", data);
+        this.dialog
+            .open(UrScheduleFormDialogComponent, { data: { data: data.event.extendedProps.schedule, action: 'edit' }})
+            .afterClosed()
+            .subscribe((result) => {
+                console.log("edit response", result);
+                if (result) {
+                    this.dirty = true;
+                    // this.roleService.update(row.level, result).subscribe(
+                    //     (data) => {
+                    //         this.snackbar.success('Edited role successfully!');
+                    //         this.refreshTable();
+                    //     },
+                    //     (error) => {
+                    //         this.snackbar.error(error);
+                    //     }
+                    // );
+                }
+            });
     }
 
+    handleEventResize(data) {
+        console.log("handleEventResize", data);
+        this.dirty = true;
+        // data.event.start;
+        // data.event.end;
+        // data.oldEvent.start;
+        // data.oldEvent.end;
+    }
+
+    handleEventDrop(data) {
+        console.log("handleEventDrop", data);
+        this.dirty = true;
+        // data.event;
+        // data.oldEvent;
+    }
+
+    deploy() {
+        this.red.deployNode(this.data).subscribe((reeponse: any) => {
+            console.log("ur-schedule deploy", reeponse);
+            if (reeponse?.rev) {
+                this.snackbar.success('Deployed successfully!');
+            }
+        });
+    }
 }
