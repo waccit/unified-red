43412a6bddc68f135aec44adddd5ddb9515fbedf
Samy Arbid <sarbid@wasocal.com>
2020-08-22 09:42:50 -0700

    Secured dashboard routes with AuthGuard and removed old MainComponent

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { MainComponent } from './main/main.component';
+// import { MainComponent } from './main/main.component';
 // import { Dashboard2Component } from './dashboard2/dashboard2.component';
 // import { Dashboard3Component } from './dashboard3/dashboard3.component';
 // import { ChartsModule as chartjsModule } from 'ng2-charts';
@@ -18,7 +18,7 @@ import { MenuPageDirective } from '../directives/menu-page.directive';
 
 @NgModule({
     declarations: [
-        MainComponent,
+        // MainComponent,
         // Dashboard2Component,
         // Dashboard3Component,
         DemoComponent,
