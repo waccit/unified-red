58d7829e32ddeee7292e7b4f1a93cb3b8372294f
Samy Arbid <sarbid@wasocal.com>
2020-08-23 16:09:01 -0700

    Added home page component to fix /d routing error

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MenuPageComponent } from './menu-page/menu-page.component';
 import { AuthGuard } from '../authentication/auth.guard';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
     {
@@ -10,8 +11,7 @@ const routes: Routes = [
         children: [
             {
                 path: '',
-                // TODO add home page component
-                // component: HomePageComponent,
+                component: HomePageComponent,
                 pathMatch: 'full',
             },
             {
