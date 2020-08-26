1dcca5544a21609ceffb4a00ff4086d74fe37858
Samy Arbid <sarbid@wasocal.com>
2020-08-25 23:14:12 -0700

    Updated ur_scheduler node to reference menu page/menu item

--- nodes/ur_schedule.js
+++ nodes/ur_schedule.js
@@ -16,15 +16,23 @@ module.exports = function (RED) {
         if (!group) {
             return;
         }
-        var subtab = RED.nodes.getNode(group.config.subtab);
-        if (!subtab) {
+        var menuPage = RED.nodes.getNode(group.config.menuPage);
+        if (!menuPage) {
             return;
         }
-        var tab = RED.nodes.getNode(subtab.config.tab);
-        if (!tab) {
+        var menuItem = RED.nodes.getNode(menuPage.config.menuItem);
+        if (!menuItem) {
             return;
         }
 
+        // menu-item tree stack (First In Last Out)
+        var menuItems = [];
+        menuItems.push(menuItem);
+        while (menuItem.config.menuItem) {
+            menuItem = RED.nodes.getNode(menuItem.config.menuItem);
+            menuItems.push(menuItem);
+        }
+        
         /*
         HELPER FUCTIONS
         */
@@ -370,8 +378,8 @@ module.exports = function (RED) {
         var done = ui.add({
             emitOnlyNewValues: false,
             node: node,
-            tab: tab,
-            subtab: subtab,
+            menuItems: menuItems,
+            menuPage: menuPage,
             group: group,
             control: {
                 id: config.id,
