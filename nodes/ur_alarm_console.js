b71c4d3c0a4c5d959159f472d3e19e8f469a74eb
Samy Arbid <sarbid@wasocal.com>
2020-09-08 11:01:06 -0700

    Added topic and severity filtering to alarm console node

--- nodes/ur_alarm_console.js
+++ nodes/ur_alarm_console.js
@@ -1,70 +1,73 @@
 module.exports = function (RED) {
-    // var ui = require('../ui')(RED);
     const alarmService = require('../api/alarms/alarm.service');
 
     function AlarmConsoleNode(config) {
         RED.nodes.createNode(this, config);
         let node = this;
+        this.topic = config.topic;
+        this.topicType = config.topicType || "json";
+        let severityFilter = {
+            "critical": config.severitycritical,
+            "alert": config.severityalert,
+            "warning": config.severitywarning,
+            "info": config.severityinfo,
+            "failover": false
+        };
 
-        // var group = RED.nodes.getNode(config.group);
-        // if (!group) {
-        //     return;
-        // }
-        // var menuPage = RED.nodes.getNode(group.config.menuPage);
-        // if (!menuPage) {
-        //     return;
-        // }
-        // var menuItem = RED.nodes.getNode(menuPage.config.menuItem);
-        // if (!menuItem) {
-        //     return;
-        // }
+        let buildTopicFilter = function(msg) {
+            let topicFilter = RED.util.evaluateNodeProperty(node.topic, node.topicType, node, msg);
+            if (!Array.isArray(topicFilter)) {
+                topicFilter = [ topicFilter ];
+            }
+            topicFilter = topicFilter.map(f => 
+                '(^' +                             //topic start
+                    f.replace(/\+/g, '[^\/]+')     //handle MQTT '+' operator to handle single level wildcards
+                     .replace(/#/g, '.*')          //handle MQTT '#' operator to handle multiple level wildcards
+                     .replace(/\//g, '\\/') +      //escape all '/'
+                '$)')                              //topic end
+                .join('|');                        //logical OR with other expressions
+            topicFilter = new RegExp(topicFilter, 'i');
+            return topicFilter;
+        };
 
-        // // menu-item tree stack (First In Last Out)
-        // var menuItems = [];
-        // menuItems.push(menuItem);
-        // while (menuItem.config.menuItem) {
-        //     menuItem = RED.nodes.getNode(menuItem.config.menuItem);
-        //     menuItems.push(menuItem);
-        // }
-
-        // var done = ui.add({
-        //     emitOnlyNewValues: false,
-        //     node: node,
-        //     menuItems: menuItems,
-        //     menuPage: menuPage,
-        //     group: group,
-        //     control: {
-        //         id: config.id,
-        //         type: 'alarm_console',
-        //         order: config.order,
-        //         width: config.width || group.config.width || 12,
-        //         // height: config.height || 1,
-        //     }
-        // });
-        // node.on('close', done);
+        let filterOnTopic = !!node.topic;
+        let topicFilter;
+        if (filterOnTopic && (node.topicType === "str" || node.topicType === "json")) {
+            topicFilter = buildTopicFilter();
+        }
 
         node.on('input', function(msg) {
-            if (msg.topic && msg.payload) {
-                if (msg.payload.action == "ack") {
-                    if (msg.payload.id) {
-                        alarmService.ackById(msg.payload.id).catch(node.error);
-                    } else {
-                        alarmService.ackByTopic(msg.topic).catch(node.error);
-                    }
+            if (!msg.topic || !msg.payload || // invalid message object
+                !severityFilter[msg.payload.severity || "failover"]) { // filter severity
+                return;
+            }
+
+            // if using msg, flow, or global variables then build the topic filter each time since these are not static values
+            if (node.topicType === "msg" || node.topicType === "flow" || node.topicType === "global") {
+                topicFilter = buildTopicFilter(msg);
+            }
+            if (filterOnTopic && !topicFilter.test(msg.topic)) { // filter topic
+                return;
+            }
+
+            if (msg.payload.action == "ack") {
+                if (msg.payload.id) {
+                    alarmService.ackById(msg.payload.id).catch(node.error);
                 } else {
-                    let alarm = {
-                        "severity": msg.payload.severity,
-                        "name": msg.payload.name,
-                        "topic": msg.topic,
-                        "value": msg.payload.value,
-                        "state": msg.payload.state,
-                        "ackreq": !!msg.payload.ackreq,
-                    };
-                    alarmService.create(alarm).catch(node.error); 
+                    alarmService.ackByTopic(msg.topic).catch(node.error);
                 }
+            } else {
+                let alarm = {
+                    "severity": msg.payload.severity,
+                    "name": msg.payload.name,
+                    "topic": msg.topic,
+                    "value": msg.payload.value,
+                    "state": msg.payload.state,
+                    "ackreq": !!msg.payload.ackreq,
+                };
+                alarmService.create(alarm).catch(node.error); 
             }
         });
-
     }
     RED.nodes.registerType('ur_alarm_console', AlarmConsoleNode);
 };
