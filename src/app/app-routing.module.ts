70aab1cd0ac07b50d1cda1013654cd9ab52fe65f
Samy Arbid <sarbid@wasocal.com>
2020-09-16 19:50:36 -0700

    app-routing clean up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,52 +11,6 @@ const routes: Routes = [
         pathMatch: 'full',
         canActivate: [AuthGuard],
     },
-    // {
-    //     path: 'dashboard',
-    //     loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
-    // },
-    // {
-    //     path: 'dashboard',
-    //     redirectTo: 'dashboard/main',
-    //     pathMatch: 'full'
-    // },
-    // {
-    //     path: 'dashboard/main',
-    //     component: MainComponent
-    // },
-    // { //TODO: review
-    //     path: 'apps',
-    //     loadChildren: () => import('./apps/apps.module').then(m => m.AppsModule)
-    // },
-    // { //TODO: review
-    //     path: 'widget',
-    //     loadChildren: () => import('./widget/widget.module').then(m => m.WidgetModule)
-    // },
-    // { //TODO: review
-    //     path: 'ui',
-    //     loadChildren: () => import('./ui/ui.module').then(m => m.UiModule)
-    // },
-    // { //TODO: review
-    //     path: 'forms',
-    //     loadChildren: () => import('./forms/forms.module').then(m => m.FormModule)
-    // },
-    // { //TODO: review
-    //     path: 'tables',
-    //     loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule)
-    // },
-    // { //TODO: review
-    //     path: 'charts',
-    //     loadChildren: () => import('./charts/charts.module').then(m => m.ChartsModule)
-    // },
-    // { //TODO: possibly for audit log
-    //     path: 'timeline',
-    //     loadChildren: () => import('./timeline/timeline.module').then(m => m.TimelineModule)
-    // },
-    // { //TODO: review
-    //     path: 'icons',
-    //     loadChildren: () => import('./icons/icons.module').then(m => m.IconsModule)
-    // },
-
     // otherwise page not found
     {
         path: '**',
