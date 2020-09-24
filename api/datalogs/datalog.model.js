0bd83dba2262a1fb11bcc3eba38633cbb41dc4fe
Samy Arbid <sarbid@wasocal.com>
2020-09-23 19:56:24 -0700

    Adding datalog models

---
+++ api/datalogs/datalog.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    timestamp: { type: Date, default: Date.now, required: true },
+    topic: { type: String, required: true },
+    value: { type: Object, required: true },
+    status: { type: String },
+    expires: { type: Date, required: true },
+});
+
+schema.index({ expires: 1 }, { expireAfterSeconds: 0 });
+
+schema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) {
+        delete ret._id;
+    },
+});
+
+module.exports = mongoose.model('Datalog', schema);
