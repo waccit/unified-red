b726d64ebc246a1eadd173de53a3879723840272
Samy Arbid <sarbid@wasocal.com>
2021-01-26 19:16:31 -0800

    Remove topic pattern from ur_schedule since it doesn't filter incoming messages

--- nodes/ur_schedule.js
+++ nodes/ur_schedule.js
@@ -393,7 +393,6 @@ module.exports = function (RED) {
                 dates: config.dates,
                 holidays: this.holidays,
                 holidaysId: config.holidays,
-                topicPattern: config.topicPattern || '',
                 access: config.access || '',
                 accessBehavior: config.accessBehavior || 'disable',
             },
