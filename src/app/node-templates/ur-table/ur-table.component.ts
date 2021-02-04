e00091749384cd4238c8954c9f18adf4920391af
Samy Arbid <sarbid@wasocal.com>
2021-02-04 00:24:51 -0800

    Add value format feature to ur-table

--- src/app/node-templates/ur-table/ur-table.component.ts
+++ src/app/node-templates/ur-table/ur-table.component.ts
@@ -7,12 +7,6 @@ import { BaseNode } from '../ur-base-node';
 	styleUrls: ['./ur-table.component.sass']
 })
 export class UrTableComponent extends BaseNode implements AfterViewInit {
-	private expressionGlobals = `
-		function interpolate(value, minIn, maxIn, minOut, maxOut) {
-			let out = minOut + (maxOut - minOut) * ((value - minIn) / (maxIn - minIn));
-			return Math.max(minOut, Math.min(maxOut, out));
-		}
-	`;
 	sortedDeviceList = [];
 	dataSource = {};
 	dataLink = {};
@@ -26,7 +20,7 @@ export class UrTableComponent extends BaseNode implements AfterViewInit {
 		super.updateValue(data);
 		if (data && data.msg && data.msg.topic && typeof data.msg.payload !== 'undefined') {
 			this.data.fields.forEach(element => {
-				let prop = element.display;
+				let prop = element.label;
 				let deviceName = null;
 				let pointName = null;
 				try {
@@ -54,23 +48,18 @@ export class UrTableComponent extends BaseNode implements AfterViewInit {
 						this.sortedDeviceList.sort();
 					}
 					let point = this.dataSource[deviceName][pointName] || {};
-					let values = element.display.split('.');
-					let result = data.msg;
 					try {
-						for (let i of values) {
-							result = result[i];
-						}
 						if (element.formatType === 'fText') {
+							point[prop] = this.formatFromData(data, element.format);
 							// Add units
 							if (element.unitType === 'unit') {
-								result += ' ' + element.unit;
-							} else if (data.msg.payload && data.msg.payload.units) {
-								result += ' ' + data.msg.payload.units;
+								point[prop] += ' ' + element.unit;
+							} else if (data.msg.payload?.units) {
+								point[prop] += ' ' + data.msg.payload.units;
 							}
-							point[prop] = this.sub(element.format, result);
 						}
 						else {
-							point[prop] = result;
+							point[prop] = this.formatFromData(data, element.format);
 						}
 					} catch (ignore) {}
 					this.dataSource[deviceName][pointName] = point;
@@ -78,32 +67,4 @@ export class UrTableComponent extends BaseNode implements AfterViewInit {
 			});
 		}
 	};
-
-	private evaluate(exp: any, value?: any) {
-		try {
-			if (typeof value !== 'undefined') {
-				exp = exp.replace(/\{x\}/ig, value); //{x} is a table expression variable not a topic variable
-			}
-			return eval('(' + exp + '); ' + this.expressionGlobals);
-		} catch (ignore) {}
-		return value;
-	}
-
-	// Example sub expressions:
-	// {x}
-	// parseInt({x} / 10)
-	// parseInt(1 + {x} / (100 / 9))
-	// Math.round({x} / 10)
-	// parseInt( interpolate({x}, 0, 100, 1, 10) )
-	// Enumeration {"0": "Offline","1": "Cooling","2": "Economizer","3": "Reheat","4": "Heat","5": "Zero CFM","6": "Air Balance","7": "Forced Damper","8": "Forced CFM","9": "Forced Reheat","10": "Forced Setpoint","11": "Forced Damper and Reheat","12": "Forced Damper and Setpoint","13": "Forced Damper, Reheat, and Setpoint","14": "Forced CFM and Reheat","15": "Forced CFM and Setpoint","16": "Forced CFM, Reheat, and Setpoint","17": "Forced Reheat and Setpoint","18": "Morning Warm-Up","19": "Ventilation"}
-	private sub(exp, payload) {
-		try {
-			if (!exp.toLowerCase().includes('{x}')) {
-				let expObj = JSON.parse(exp);
-				return expObj[payload];
-			}
-		} catch (ignore) {}
-		let result = this.evaluate(exp, payload);
-		return result;
-	}
 }
