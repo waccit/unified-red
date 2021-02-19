ed4215c2ce99b33d36cbe59f0b214ee273493ba1
Nick Lee <nlee@wasocal.com>
2021-02-18 22:40:00 -0800

    cleanup

--- nodes/ur_link.js
+++ nodes/ur_link.js
@@ -8,17 +8,7 @@ module.exports = function (RED) {
         var folders = [];
         config.icon = 'ti-' + config.icon;
 
-        // console.log('node: ', node);
-        // console.log('config: ', config);
-
         var { folders, ...ignored } = ui.makeMenuTree(RED, config);
-        // console.log('ignored: ', ignored);
-        // console.log('folders: ', folders);
-
-        // if (config.folder) {
-        //     folder = RED.nodes.getNode(config.folder);
-        //     console.log('folder: ', folder);
-        // }
 
         var done = ui.addLink(config.id, config.name, folders, config.link, config.icon, config.order, config.target);
 
