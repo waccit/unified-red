da844b93c2e969670fe1708d6bae307434e22993
Maxwell Christy <mchristy@wasocal.com>
2025-06-10 11:20:36 -0700

    fix: fix jstree styling and add buttons on hover

--- src/assets/js/ur_jstree.js
+++ src/assets/js/ur_jstree.js
@@ -1,4 +1,28 @@
-(function() {
+(function () {
+    // Inject CSS for jsTree styling
+    function injectJsTreeStyles() {
+        if (document.getElementById('ur-jstree-styles')) {
+            return; // Styles already injected
+        }
+
+        const style = document.createElement('style');
+        style.id = 'ur-jstree-styles';
+        style.textContent = `
+            /* Remove left margin from jstree container ul */
+            ul.jstree-container-ul.jstree-children.jstree-wholerow-ul.jstree-no-dots {
+                margin-left: 0 !important;
+            }
+        `;
+        document.head.appendChild(style);
+    }
+
+    // Inject styles when DOM is ready
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', injectJsTreeStyles);
+    } else {
+        injectJsTreeStyles();
+    }
+
     function extractNodesFromConfig(nodeType, parentType, parentNode) {
         var nodesArray = [];
         RED.nodes.eachConfig(function (node) {
@@ -25,7 +49,7 @@
             text: folderNode.name,
             id: folderNode.id,
             type: 'folder',
-            children: []
+            children: [],
         };
 
         var pages = extractNodesFromConfig('ur_page', 'folder', folderNode);
@@ -34,7 +58,7 @@
                 text: page.name,
                 id: page.id,
                 type: 'page',
-                children: []
+                children: [],
             };
             var groups = extractNodesFromConfig('ur_group', 'page', page);
             groups.forEach(function (group) {
@@ -42,14 +66,14 @@
                     text: group.name,
                     id: group.id,
                     type: 'group',
-                    children: []
+                    children: [],
                 };
                 var tabs = extractNodesFromConfig('ur_tab', 'group', group);
                 tabs.forEach(function (tab) {
                     groupWithChildren.children.push({
                         text: tab.name,
                         id: tab.id,
-                        type: 'tab'
+                        type: 'tab',
                     });
                 });
                 pageWithChildren.children.push(groupWithChildren);
@@ -76,35 +100,13 @@
         return rootFoldersArray;
     }
 
-    function createEditButton() {
-        let editButton = $(
-            '<a href="#" class="editor-button editor-button-small nr-db-sb-list-header-button"><i class="fa fa-pencil"></i> ' +
-            'Edit' +
-            '</a>'
-        );
-
-        editButton.on('click', function (evt) {
-            console.log('evt', evt);
-            if (selectedTab) {
-                let tabNode = RED.nodes.node(selectedTab.id); // Assuming RED.nodes.node() gets the tab node by id
-                if (tabNode) {
-                    RED.editor.editConfig('', tabNode.type, tabNode.id);
-                }
-            }
-            evt.stopPropagation();
-            evt.preventDefault();
-        });
-
-        return editButton;
-    }
-
     function scrollToNode(nodeId) {
         let treeInstance = $.jstree.reference('#jstree');
         if (!treeInstance) {
-            console.error("jsTree instance not available for scrollToNode");
+            console.error('jsTree instance not available for scrollToNode');
             return;
         }
-        
+
         let node = treeInstance.get_node(nodeId, true);
 
         if (node && node.length) {
@@ -119,94 +121,94 @@
     function getPathToNode(nodeId) {
         let treeInstance = $.jstree.reference('#jstree');
         if (!treeInstance) {
-            console.error("jsTree instance not available for getPathToNode");
-            return "None";
+            console.error('jsTree instance not available for getPathToNode');
+            return 'None';
         }
-        
+
         let pathArray = [];
         let currentNode = treeInstance.get_node(nodeId);
-        
+
         if (!currentNode) {
-            return "None";
+            return 'None';
         }
-        
+
         while (currentNode && currentNode.id !== '#') {
             pathArray.unshift(currentNode.text);
             currentNode = treeInstance.get_node(currentNode.parent);
         }
-        
+
         return pathArray.join('][');
     }
-    
+
     function initializeJsTree(tabId) {
-        // Check if jstree container exists
         if ($('#jstree').length === 0) {
             console.error("Element with ID 'jstree' not found in the DOM");
             return;
         }
+        selectedTab = tabId;
 
         var rootFolders = extractRootFolders();
-        var foldersTreeData = rootFolders.map(folder => extractFolderChildren(folder));
-        
-        // Check if jsTree is already initialized
+        var foldersTreeData = rootFolders.map((folder) => extractFolderChildren(folder));
+
         if ($.jstree.reference('#jstree')) {
-            // Destroy the existing instance before recreating
             $('#jstree').jstree('destroy');
         }
 
         // Initial jsTree setup
         $('#jstree').jstree({
             'core': {
-                'data': foldersTreeData
+                'data': foldersTreeData,
+                'dblclick_toggle': false,
             },
-            'plugins': ['types', 'search'],
+            'multiple': false,
+            'plugins': ['types', 'search', 'wholerow'],
             'types': {
                 'default': {
                     // 'icon': 'fa fa-folder-o',
                 },
                 'folder': {
-                    'icon': 'fa fa-folder-o', 
+                    'icon': 'fa fa-folder-o',
                 },
                 'page': {
-                    'icon': 'fa fa-file-o', 
+                    'icon': 'fa fa-file-o',
                 },
                 'group': {
-                    'icon': 'fa fa-window-maximize',  
+                    'icon': 'fa fa-window-maximize',
                 },
                 'tab': {
-                    'icon': 'fa fa-columns', 
-                }
+                    'icon': 'fa fa-columns',
+                },
             },
             'search': {
                 // 'show_only_matches': true
-            }
+            },
         });
 
-        $('#jstree').on("select_node.jstree", function (e, data) {
+        // handle click on a node (tab or non-tab)
+        $('#jstree').on('select_node.jstree', function (e, data) {
             if (data.node.type === 'tab') {
                 selectedTab = data.node;
                 let path = getPathToNode(selectedTab.id);
                 $('#selectedTabDisplay').text(`Selected Tab: [${path}]`);
             } else {
-                // Prevents deselecting the current tab when clicking on non-tab nodes
                 var instance = $.jstree.reference('#jstree');
-                if (instance) {
+                if (instance.is_open(data.node)) {
+                    instance.close_node(data.node);
+                } else {
+                    instance.open_node(data.node);
+                }
+                if (selectedTab) {
                     instance.deselect_node(data.node);
-                    if (selectedTab) {
-                        instance.select_node(selectedTab.id);
-                    }
+                    instance.select_node(selectedTab.id);
                 }
             }
-            console.log("Selected node: ", data.node);
-        });
-
-        $('#jstree').on("changed.jstree", function (e, data) {
-            console.log("Changed Selected Node: ", data.selected);
         });
 
         var to = false;
         $('#treeSearch').keyup(function () {
-            if (to) { clearTimeout(to); }
+            if (to) {
+                clearTimeout(to);
+            }
             to = setTimeout(function () {
                 var v = $('#treeSearch').val();
                 var instance = $.jstree.reference('#jstree');
@@ -216,49 +218,96 @@
             }, 250);
         });
 
-        console.log("Retrieved tab ID: ", tabId);
-        // Restore selected tab when the node is reopened, or select the first tab if none is selected
-        $('#jstree').on('ready.jstree', function () {
-            var instance = $.jstree.reference('#jstree');
-            if (!instance) {
-                console.error("jsTree instance not available");
-                return;
-            }
+        $('#jstree').on('hover_node.jstree', function (e, data) {
+            console.log('data', data.node.type);
+            $('.jstree-hover-button').remove();
 
-            if (tabId) {
-                instance.select_node(tabId);
-                var selectedNode = instance.get_node(tabId);
-                console.log("Selected node:", selectedNode);
-                if (selectedNode) {
-                    let path = getPathToNode(selectedNode.id);
-                    $('#selectedTabDisplay').text(`Selected Tab: [${path}]`);
-                    setTimeout(() => {
-                        scrollToNode(tabId); // Auto-scroll to the selected tab with delay
-                    }, 200);
-                } else {
-                    $('#selectedTabDisplay').text('Selected Tab: None');
-                }
-            } else {
-                // If no tab is selected, default to the first available tab
-                var rootNode = instance.get_node('#');
-                if (rootNode && rootNode.children_d) {
-                    var firstTab = rootNode.children_d.find(id => {
-                        var node = instance.get_node(id);
-                        return node && node.type === 'tab';
+            let buttons = [];
+
+            switch (data.node.type) {
+                case 'folder':
+                    break;
+                case 'page':
+                    break;
+                case 'group':
+                    const node_config = {
+                        'id': RED.util.generateId(),
+                        'type': 'ur_tab',
+                        'name': 'Tab 1',
+                        'order': 1,
+                        'group': data.node.id,
+                        'disabled': false,
+                        'hidden': false,
+                        'access': '',
+                    };
+
+                    let addGroupButton = $(
+                        `<a href="#" class="jstree-hover-button editor-button editor-button-small nr-db-sb-list-header-button"><i class="fa fa-plus"></i>tab</a>`
+                    );
+                    addGroupButton.on('click', function (evt) {
+                        let tabNode = RED.nodes.node(data.node.id);
+                        if (tabNode) {
+                            RED.createNode(node_config);
+                        }
                     });
-                    if (firstTab) {
-                        instance.select_node(firstTab);
-                        let path = getPathToNode(firstTab);
+                    buttons.push(addGroupButton);
+
+                    break;
+                case 'tab':
+                    break;
+            }
+            let editButton = $(
+                `<a href="#" class="jstree-hover-button editor-button editor-button-small nr-db-sb-list-header-button"><i class="fa fa-pencil"></i> </a>`
+            );
+            editButton.on('click', function (evt) {
+                let tabNode = RED.nodes.node(data.node.id);
+                if (tabNode) {
+                    RED.editor.editConfig('', tabNode.type, tabNode.id);
+                }
+            });
+            buttons.push(editButton);
+
+            var $node = $('#' + data.node.id);
+            var $anchor = $node.children('.jstree-anchor');
+            $anchor.css('position', 'relative');
+            $anchor.append(buttons);
+        });
+
+        $('#jstree').on('dehover_node.jstree', function () {
+            $('.jstree-hover-button').remove();
+        });
+
+        // Restore selected tab when the node is reopened, or select the first tab if none is selected
+        $('#jstree').on(
+            'ready.jstree',
+            function () {
+                var instance = $.jstree.reference('#jstree');
+                if (!instance) {
+                    console.error('jsTree instance not available');
+                    return;
+                }
+
+                if (tabId) {
+                    instance.select_node(tabId);
+                    var selectedNode = instance.get_node(tabId);
+                    if (selectedNode) {
+                        let path = getPathToNode(selectedNode.id);
                         $('#selectedTabDisplay').text(`Selected Tab: [${path}]`);
                         setTimeout(() => {
-                            scrollToNode(firstTab); // Auto-scroll to the first available tab with delay
-                        }, 200);                        
+                            scrollToNode(tabId);
+                        }, 200);
                     } else {
                         $('#selectedTabDisplay').text('Selected Tab: None');
                     }
+                } else {
+                    // If no tab is selected, default to the first available tab
+                    var rootNode = instance.get_node('#');
+                    if (rootNode && rootNode.children_d) {
+                        $('#selectedTabDisplay').text('Selected Tab: None');
+                    }
                 }
-            }
-        }.bind(this)); // Ensures info is saved
+            }.bind(this)
+        );
 
         // Update the selected tab display outside of the ready event
         if (tabId) {
@@ -275,9 +324,6 @@
         } else {
             $('#selectedTabDisplay').text('Selected Tab: None');
         }
-
-        // Add the 'Open' button next to the 'Selected Tab' display
-        $('#editButtonContainer').append(createEditButton());
     }
 
     // Exposes functions to global scope for use in HTML scripts
